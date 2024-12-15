91f7182d70e41a7374188c1ba98e56f4075b2d6a
danielricci <thedanny09@icloud.com>
2024-12-15 16:39:05 -0500

    Initial commit

---
+++ flushfinder/flushfinder/flushfinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  flushfinderApp.swift
+//  flushfinder
+//
+//  Created by Daniel Ricci on 2024-12-15.
+//
+
+import SwiftUI
+
+@main
+struct flushfinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
